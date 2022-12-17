6dcc18c034aadbb769d15975db128e04a7bd1ba3
geethaka-Nrs <nrs.geethakanawarathna.123@gmail.com>
2022-12-18 02:29:37 +0530

    creating a node server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')// framework
+const mongoose = require('mongoose')//use to work with mongodb
+const morgan = require('morgan')// important for deploy
+const bodyParser = require('body-parser')//pass the request and submit response and request
+
+mongoose.connect('mongodb://localhost:27017/fuelapp',{useNewUrlParser:true,useUnifiedTopology:true})
+const db = mongoose.connection
+
+db.on('error',(err) =>{
+    console.log(err)
+})
+
+db.once('open', ()=> {
+    console.log('Database Connection Established!')
+})
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
