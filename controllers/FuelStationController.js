376cf742fcd8e2d5848a0e35c0db3b883422365a
Lahiru Imanshana <83973661+Lahiru-Ima@users.noreply.github.com>
2023-01-06 23:17:15 +0530

    Fuel Station added (#9)
    * Add fuel station endpoints
    
    * Added endpoints and add routes
    
    * Fix errors

---
+++ controllers/FuelStationController.js
@@ -0,0 +1,71 @@
+const FuelStation = require("../models/FuelStation")
+const { response } = require('express')
+
+const addFuelStation = async (req, res) => {
+    const fuelStation = req.body
+    const newFuelStation = new FuelStation(fuelStation)
+    try {
+        await newFuelStation.save()
+        res.status(201).json(newFuelStation)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+
+const getFuelStationByDealer = async (req, res) => {
+    const dealer = req.query.dealer
+    try {
+        const fuelStation = await FuelStation.find({ dealer: dealer })
+        res.status(200).json(fuelStation)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+const addFuelStatusToFuelStation = async (req, res) => {
+    const _id = req.params.id
+    const body = req.body
+    const stationObj = body
+
+    try {
+        console.log(stationObj)
+        const newFuelStatus = await FuelStation.findByIdAndUpdate(
+            { _id },
+            stationObj
+        )
+        console.log("success")
+        res.status(201).json(newFuelStatus)
+    } catch (error) {
+        console.log(error)
+        res.status(409).json({ message: error.message })
+    }
+}
+
+const getAllFuelStations = async (req, res) => {
+    try {
+        const fuelStation = await FuelStation.find()
+        res.status(200).json(fuelStation)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+function convertToJsDate(javaDate) {
+    const dateArr = javaDate.split(" ")
+    const convertedDate = "20" + dateArr[0] + "T" + dateArr[1]
+    return convertedDate
+}
+
+const getFuelStation = async (req, res) => {
+    try {
+        const id = req.params.id
+        const fuelStation = await FuelStation.findById(id)
+        res.status(200).json(fuelStation)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+};
+
+module.exports = {
+    addFuelStation, getFuelStationByDealer, addFuelStatusToFuelStation, getAllFuelStations, getFuelStation
+}
---
+++ routes/fuelStation.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const fuelStationController = require('../controllers/FuelStationController')
+
+router.get('/',fuelStationController.getAllFuelStations).post('/',fuelStationController.addFuelStation)
+router.get('/single/:id',fuelStationController.getFuelStation)
+router.get('/dealer',fuelStationController.getFuelStationByDealer)
+router.post('/addStatus/:id',fuelStationController.addFuelStatusToFuelStation)
+
+module.exports = router
