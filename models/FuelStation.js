2f8ecf4b9331fbaadc5af284b53ca63c1688a7f1
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-06 22:14:41 +0530

    Add fuel station endpoints

---
+++ models/FuelStation.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const fuelStationSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+      },
+      owner: {
+        type: String,
+        trim: true,
+      },
+      location: {
+        type: String,
+        trim: true,
+      },
+      petrolStatus: {
+        type: String,
+        trim: true,
+      },
+      petrolArrivalTime: {
+        type: Date,
+        trim: true,
+      },
+      petrolFinishedTime: {
+        type: Date,
+        trim: true,
+      },
+      dieselStatus: {
+        type: String,
+        trim: true,
+      },
+      dieselArrivalTime: {
+        type: Date,
+        trim: true,
+      },
+      dieselFinishedTime: {
+        type: Date,
+        trim: true,
+      },
+},{timestamps:true})
+
+const FuelStation = mongoose.model('FuelStatus',fuelStationSchema)
+module.exports = FuelStation
