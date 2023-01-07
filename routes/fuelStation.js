a91bc0e0cee177de1176210ab1e59a0c18e11895
Lahiru Imanshana <83973661+Lahiru-Ima@users.noreply.github.com>
2023-01-07 11:28:04 +0530

    Fix models errors (#10)
    * Fix models errors
    
    * Fix route errors

--- routes/fuelStation.js
+++ routes/fuelStation.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const fuelStationController = require('../controllers/FuelStationController')
 
-router.get('/',fuelStationController.getAllFuelStations).post('/',fuelStationController.addFuelStation)
+router.get('/',fuelStationController.getAllFuelStations)
+router.post('/',fuelStationController.addFuelStation)
 router.get('/single/:id',fuelStationController.getFuelStation)
 router.get('/dealer',fuelStationController.getFuelStationByDealer)
 router.post('/addStatus/:id',fuelStationController.addFuelStatusToFuelStation)
