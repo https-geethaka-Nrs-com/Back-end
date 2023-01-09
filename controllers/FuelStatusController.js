37e2d0b70ab25c915c7b10b418257b43abd89c9f
Lahiru-Ima <lahiru.sl24@gmail.com>
2023-01-09 17:57:26 +0530

    Fix fuel status errors

--- controllers/FuelStatusController.js
+++ controllers/FuelStatusController.js
@@ -8,13 +8,28 @@ const addFuelStatus = async (req, res) => {
     const newFuelStatus = new FuelStatus(fuelStatus)
     try {
       await newFuelStatus.save()
-      res.status(200).json(newFuelStatus)
+      res.status(201).json(newFuelStatus)
     } catch (error) {
       res.status(409).json({ message: error.message })
     }
   }
 
+const updateFuelStatusByName = async (req, res) => {
+    let fuelStatus = req.body;
+    try {
+        const updatedFuelStatus = await FuelStatus.findOneAndUpdate(
+        { fuelStation: fuelStatus.name },
+        fuelStatus,
+        { new: true }
+        );
+        res.status(200).json(updatedFuelStatus);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+
+}
+
 module.exports = {
-    addFuelStatus
+    addFuelStatus,updateFuelStatusByName
 }
 
--- routes/fuelStatus.js
+++ routes/fuelStatus.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const fuelStatusController = require('../controllers/FuelStatusController')
+const updateFuelStatusByName = require('../controllers/FuelStatusController')
 
-router.post('/',fuelStatusController.addFuelStatus)
+router.post('/', fuelStatusController.addFuelStatus)
+router.post("/update", updateFuelStatusByName.updateFuelStatusByName)
 
 module.exports = router
